1242c42ddcb0d5b820400aafd67e5b75b26493da
Jamie Balfour <jamie@balfour.scot>
2026-02-15 09:33:58 +0000

    Built and tested the first version of this plugin.

--- src/Plugin.java
+++ src/Plugin.java
@@ -1,4 +1,126 @@
-package PACKAGE_NAME;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.DatabaseBuilder;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
+import jamiebalfour.zpe.core.ZPEFunction;
+import jamiebalfour.zpe.core.ZPERuntimeEnvironment;
+import jamiebalfour.zpe.core.ZPEStructure;
+import jamiebalfour.zpe.interfaces.ZPECustomFunction;
+import jamiebalfour.zpe.interfaces.ZPELibrary;
+import jamiebalfour.zpe.interfaces.ZPEType;
+import jamiebalfour.zpe.types.ZPEBoolean;
 
-public class Plugin {
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Plugin implements ZPELibrary {
+
+  @Override
+  public Map<String, ZPECustomFunction> getFunctions() {
+    HashMap<String, ZPECustomFunction> functions = new HashMap<>();
+    functions.put("open_access_file", new open_access_file());
+    return functions;
+  }
+
+  @Override
+  public Map<String, Class<? extends ZPEStructure>> getObjects() {
+    return Map.of();
+  }
+
+  @Override
+  public boolean supportsWindows() {
+    return true;
+  }
+
+  @Override
+  public boolean supportsMacOs() {
+    return true;
+  }
+
+  @Override
+  public boolean supportsLinux() {
+    return true;
+  }
+
+  @Override
+  public String getName() {
+    return "libMSAccess";
+  }
+
+  @Override
+  public String getVersionInfo() {
+    return "1.0";
+  }
+
+  public static void main(String[] args) {
+    Database db = null;
+    try {
+      db = DatabaseBuilder.open(new File("/Users/jamiebalfour/Downloads/Customer.accdb"));
+      for (String tableName : db.getTableNames()) {
+        Table table = db.getTable(tableName);
+
+        for (Row row : table) {
+          System.out.println(row.get("foreName"));
+        }
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
+
+  class open_access_file implements ZPECustomFunction {
+
+    @Override
+    public int getRequiredPermissionLevel() {
+      return 3;
+    }
+
+    @Override
+    public String getManualHeader() {
+      return "Opens an Microsoft Access database file and returns an MSAccess object";
+    }
+
+    @Override
+    public String getManualEntry() {
+      return "";
+    }
+
+    @Override
+    public String[] getParameterNames() {
+      return new String[]{"path"};
+    }
+
+    @Override
+    public ZPEType MainMethod(HashMap<String, Object> parameters, ZPERuntimeEnvironment runtime, ZPEFunction currentFunction) {
+
+      String path = (String) parameters.get("path").toString();
+
+      if(new File(path).exists()){
+       try{
+         Database db = DatabaseBuilder.open(new File("/Users/jamiebalfour/Downloads/Customer.accdb"));
+
+         return new ZPEMSAccess(runtime, currentFunction, db);
+
+       } catch (IOException e) {
+         return ZPEBoolean.FALSE();
+       }
+      }
+
+      return ZPEBoolean.FALSE();
+
+    }
+
+    @Override
+    public byte getReturnType() {
+      return 0;
+    }
+
+    @Override
+    public int getMinimumParameters() {
+      return 0;
+    }
+  }
 }
--- src/ZPEMSAccess.java
+++ src/ZPEMSAccess.java
@@ -1,4 +1,236 @@
-package PACKAGE_NAME;
+import com.healthmarketscience.jackcess.Column;
+import com.healthmarketscience.jackcess.Database;
+import com.healthmarketscience.jackcess.Row;
+import com.healthmarketscience.jackcess.Table;
+import jamiebalfour.generic.JBBinarySearchTree;
+import jamiebalfour.zpe.core.ZPEObject;
+import jamiebalfour.zpe.core.ZPERuntimeEnvironment;
+import jamiebalfour.zpe.core.ZPEStructure;
+import jamiebalfour.zpe.exceptions.ExitHalt;
+import jamiebalfour.zpe.exceptions.IncorrectDataTypeException;
+import jamiebalfour.zpe.exceptions.ZPERuntimeException;
+import jamiebalfour.zpe.interfaces.ZPEObjectNativeMethod;
+import jamiebalfour.zpe.interfaces.ZPEPropertyWrapper;
+import jamiebalfour.zpe.interfaces.ZPEType;
+import jamiebalfour.zpe.types.ZPEBoolean;
+import jamiebalfour.zpe.types.ZPEList;
+import jamiebalfour.zpe.types.ZPEString;
 
-public class ZPEMSAccess {
+import java.io.IOException;
+
+public class ZPEMSAccess extends ZPEStructure {
+
+  Database db;
+  ZPERuntimeEnvironment runtime;
+
+  protected ZPEMSAccess(ZPERuntimeEnvironment z, ZPEPropertyWrapper parent, Database db) {
+    super(z, parent, "ZPEMSAccess");
+    this.db = db;
+
+    addNativeMethod("get_tables", new get_tables_Command());
+    addNativeMethod("get_table", new get_table_Command());
+  }
+
+  public class get_tables_Command implements ZPEObjectNativeMethod {
+
+    @Override
+    public String[] getParameterNames() {
+      return new String[0];
+    }
+
+    @Override
+    public String[] getParameterTypes() {
+      return new String[0];
+    }
+
+    @Override
+    public ZPEType MainMethod(JBBinarySearchTree<String, ZPEType> parameters, ZPEObject parent) throws ZPERuntimeException, ExitHalt, IncorrectDataTypeException {
+
+      ZPEList output = new ZPEList();
+      try {
+        for (String tableName : db.getTableNames()) {
+          output.add(new ZPEString(tableName));
+        }
+      } catch (IOException e) {
+        //Ignore
+      }
+
+
+      return output;
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+      return 0;
+    }
+
+    @Override
+    public String getName() {
+      return "get_tables";
+    }
+  }
+
+  public class get_table_Command implements ZPEObjectNativeMethod {
+
+    @Override
+    public String[] getParameterNames() {
+      return new String[]{"table_name"};
+    }
+
+    @Override
+    public String[] getParameterTypes() {
+      return new String[]{"string"};
+    }
+
+    @Override
+    public ZPEType MainMethod(JBBinarySearchTree<String, ZPEType> parameters, ZPEObject zpeObject) throws ZPERuntimeException, ExitHalt, IncorrectDataTypeException {
+      String tableName = parameters.get("table_name").toString();
+      try {
+        Table table = db.getTable(tableName);
+        return new ZPEMSAccessTable(runtime, zpeObject, table);
+      } catch (IOException e) {
+        return ZPEBoolean.FALSE();
+      }
+
+
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+      return 0;
+    }
+
+    @Override
+    public String getName() {
+      return "get_table";
+    }
+  }
+
+  public class ZPEMSAccessTable extends ZPEStructure {
+
+    Table table;
+
+    protected ZPEMSAccessTable(ZPERuntimeEnvironment z, ZPEPropertyWrapper parent, Table table) {
+      super(z, parent, "MSAccessTable");
+      this.table = table;
+
+      addNativeMethod("get_rows", new get_rows_Command());
+      addNativeMethod("get_field_names", new get_field_names_Command());
+    }
+
+    public class get_field_names_Command implements ZPEObjectNativeMethod {
+
+      @Override
+      public String[] getParameterNames() {
+        return new String[0];
+      }
+
+      @Override
+      public String[] getParameterTypes() {
+        return new String[0];
+      }
+
+      @Override
+      public ZPEType MainMethod(JBBinarySearchTree<String, ZPEType> jbBinarySearchTree, ZPEObject zpeObject) throws ZPERuntimeException, ExitHalt, IncorrectDataTypeException {
+
+        ZPEList output = new ZPEList();
+
+        for(Column c : table.getColumns()){
+          output.add(new ZPEString(c.getName()));
+        }
+
+        return output;
+
+      }
+
+      @Override
+      public int getRequiredPermissionLevel() {
+        return 0;
+      }
+
+      @Override
+      public String getName() {
+        return "get_field_names";
+      }
+    }
+
+    public class get_rows_Command implements ZPEObjectNativeMethod {
+      @Override
+      public String[] getParameterNames() {
+        return new String[0];
+      }
+
+      @Override
+      public String[] getParameterTypes() {
+        return new String[0];
+      }
+
+      @Override
+      public ZPEType MainMethod(JBBinarySearchTree<String, ZPEType> jbBinarySearchTree, ZPEObject zpeObject) throws ZPERuntimeException, ExitHalt, IncorrectDataTypeException {
+
+        ZPEList output = new ZPEList();
+
+        for(Row r : table){
+          output.add(new ZPEMSAccessRow(runtime, zpeObject, r));
+        }
+
+        return output;
+
+      }
+
+      @Override
+      public int getRequiredPermissionLevel() {
+        return 0;
+      }
+
+      @Override
+      public String getName() {
+        return "get_rows";
+      }
+    }
+  }
+
+  public class ZPEMSAccessRow extends ZPEStructure {
+
+    Row row;
+
+    protected ZPEMSAccessRow(ZPERuntimeEnvironment z, ZPEPropertyWrapper parent, Row row) {
+      super(z, parent, "MSAccessRow");
+      this.row = row;
+
+      addNativeMethod("get_column", new get_column_Command());
+
+    }
+
+    public ZPEType get(String columnName){
+      return new ZPEString(row.get(columnName).toString());
+    }
+
+    public class get_column_Command implements ZPEObjectNativeMethod {
+      @Override
+      public String[] getParameterNames() {
+        return new String[]{"column_name"};
+      }
+
+      @Override
+      public String[] getParameterTypes() {
+        return new String[]{"string"};
+      }
+
+      @Override
+      public ZPEType MainMethod(JBBinarySearchTree<String, ZPEType> parameters, ZPEObject zpeObject) throws ZPERuntimeException, ExitHalt, IncorrectDataTypeException {
+        return get(parameters.get("column_name").toString());
+      }
+
+      @Override
+      public int getRequiredPermissionLevel() {
+        return 0;
+      }
+
+      @Override
+      public String getName() {
+        return "get_column";
+      }
+    }
+  }
 }
